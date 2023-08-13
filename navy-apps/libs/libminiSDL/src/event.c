a6e813376806f717c0614ff9093cddd291f4a71a
YSXCC <wshysxcc@outlook.com>
2023-08-13 17:49:17 +0800

    finish nterm

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -22,8 +22,10 @@ int SDL_PollEvent(SDL_Event *ev) {
           ev->key.type = SDL_KEYUP;
       }
 
+      int flag = 0;
       for (unsigned i = 0; i < sizeof(keyname) / sizeof(keyname[0]); ++i) {
-          if (strncmp(buf + 3, keyname[i], strlen(buf) - 4) == 0) {
+          if (strncmp(buf + 3, keyname[i], strlen(buf) - 4) == 0 && strlen(keyname[i]) == strlen(buf) - 4) {
+              flag = 1;
               ev->key.keysym.sym = i;
               break;
           }
@@ -32,8 +34,7 @@ int SDL_PollEvent(SDL_Event *ev) {
       free(buf);
       return 1;
   } else {
-      ev->key.type = SDL_USEREVENT; // avoid too many `Redirecting file open ...`
-      ev->key.keysym.sym = 0;
+      return 0;
   }
 
   free(buf);
@@ -41,8 +42,28 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  SDL_PollEvent(event);
-  return 1;
+    unsigned buf_size = 32;
+    char *buf = (char *)malloc(buf_size * sizeof(char));
+
+    while (NDL_PollEvent(buf, buf_size) == 0); // wait ...
+
+    if (strncmp(buf, "kd", 2) == 0)
+        event->key.type = SDL_KEYDOWN;
+    else
+        event->key.type = SDL_KEYUP;
+
+
+    int flag = 0;
+    for (unsigned i = 0; i < sizeof(keyname) / sizeof(keyname[0]); ++i) {
+        if (strncmp(buf + 3, keyname[i], strlen(buf) - 4) == 0 && strlen(keyname[i]) == strlen(buf) - 4) {
+            flag = 1;
+            event->key.keysym.sym = i;
+            break;
+        }
+    }
+
+    free(buf);
+    return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
